be017653a3d093a9ad632cd4d07649352c4bd650
castawaypirate <apollonas@csd.auth.gr>
2024-03-30 17:06:22 +0200

    Load posts upon navigating to dashboard
    Display functionality is pending implementation.

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,34 @@
 import config from "./config.js";
 
 async function loadPosts() {
-    // console.log("HA");
+    const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      };
+    
+      fetch(`${config.apiUrl}/posts/getPosts`, options)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok.');
+          }
+          return response.json();
+        })
+        .then(result => {
+          if (result.success) {
+            console.log(result);
+          } else {
+            console.log(result);
+          }
+        })
+        .catch(error => {
+          console.error("Error: ", error);
+        });
 }
 
 if (document.readyState !== 'loading') {
-    const user = loadPosts();
+    loadPosts();
 } else {
     document.addEventListener('DOMContentLoaded', loadPosts);
 }
